3e6d5650df9a7f344936582d5e86bc2198fb71c3
dalbano96 <dalbano96@gmail.com>
2018-06-28 16:00:42 -0700

    Added UN Reno Data

--- code/AERONET-AOD-main.R
+++ code/AERONET-AOD-main.R
@@ -73,14 +73,44 @@ ggplot(subset(testdf, DateTime.GMT > "2016-01-01" & DateTime.GMT < "2016-12-31")
   geom_point() +
   ggtitle("2016")
 
+
 #--------------------------------------------------------------#
-# Map site locations for AERONET AOD sites
+# Read AOD csv to data frame (Reno, NV)
 #--------------------------------------------------------------#
-leaflet(unique(select(hourly.AOD.AMES.14_17, c(Site_Latitude.Degrees., Site_Longitude.Degrees., AERONET_Site_Name)))) %>%
-  addCircles(~Site_Longitude.Degrees., ~Site_Latitude.Degrees.,
-             label = ~paste("Site Name: ", AERONET_Site_Name)) %>%
-  addTiles() %>%
-  addProviderTiles(providers$CartoDB.Positron)
+# 1) Importing AOD datasets
+hourly.AOD.Reno.14_17 <- NULL
+hourly.AOD.Reno.14_17 <- read.csv("data/20140101_20171231_Univ_of_Nevada-Reno/20140101_20171231_Univ_of_Nevada-Reno.csv", stringsAsFactors = FALSE)
+
+# 2) Changing date format of Date.GMT. Corrects to "yyyy-mm-dd"
+hourly.AOD.Reno.14_17$Date.dd.mm.yyyy. <- as.Date(hourly.AOD.Reno.14_17$Date.dd.mm.yyyy., format = "%m/%d/%y")
+
+# 3) Rename date/time columns to Date.GMT and Time.GMT since those units were measured in GMT time zone
+hourly.AOD.Reno.14_17 <- rename(hourly.AOD.Reno.14_17, Date.GMT = Date.dd.mm.yyyy.)
+hourly.AOD.Reno.14_17 <- rename(hourly.AOD.Reno.14_17, Time.GMT = Time.hh.mm.ss.)
+
+# 4) Joining date and time into a new column "DateTime.GMT"
+hourly.AOD.Reno.14_17$DateTime.GMT <- as.POSIXct(paste(hourly.AOD.Reno.14_17$Date.GMT,
+                                                       hourly.AOD.Reno.14_17$Time.GMT),
+                                                 format = "%Y-%m-%d %H:%M:%S")
+
+# 5) Setting all -999 to NA TODO: There has to be a better way to format this.
+hourly.AOD.Reno.14_17 <- hourly.AOD.Reno.14_17 %>%
+  mutate(AOD_1640nm = replace(AOD_1640nm, AOD_1640nm == -999, NA)) %>%
+  mutate(AOD_1020nm = replace(AOD_1020nm, AOD_1020nm == -999, NA)) %>%
+  mutate(AOD_870nm = replace(AOD_870nm, AOD_870nm == -999, NA)) %>%
+  mutate(AOD_675nm = replace(AOD_675nm, AOD_675nm == -999, NA)) %>%
+  mutate(AOD_500nm = replace(AOD_500nm, AOD_500nm == -999, NA)) %>%
+  mutate(AOD_440nm = replace(AOD_440nm, AOD_440nm == -999, NA)) %>%
+  mutate(AOD_380nm = replace(AOD_380nm, AOD_380nm == -999, NA)) %>%
+  mutate(AOD_340nm = replace(AOD_340nm, AOD_340nm == -999, NA)) %>%
+  mutate(X440.870_Angstrom_Exponent = replace(X440.870_Angstrom_Exponent, X440.870_Angstrom_Exponent == -999, NA))
+
+# Graph!
+hourly.AOD.Reno.14_17 %>%
+  na.omit() %>%
+  subset(DateTime.GMT >= "2015-08-01 00:00" & DateTime.GMT <= "2015-12-31 23:59") %>%
+  ggplot(aes(x = Time.GMT, y = X440.870_Angstrom_Exponent)) +
+  geom_point(aes(group = as.character(Date.GMT)))
 
 
 # # Graph of AOD
