5fba3e88dbdb7bb24531e126d84bd91a60f039d4
dalbano96 <dalbano96@gmail.com>
2018-06-29 14:12:46 -0700

    commit

--- code/map_functions.R
+++ code/map_functions.R
@@ -17,7 +17,7 @@ aeronet.sites <- read.delim("data/aeronet_locations_v3_2015_lev20.txt", header =
 #--------------------------------------------------------------#
 leaflet(unique(select(subset(hourly.pm25.FRM.14_17), c(Longitude, Latitude, Site.Num, County.Name)))) %>%
   addCircles(~Longitude, ~Latitude,
-             label = ~paste("Site Num: ", Site.Num, ", ",
+             label = ~paste("[EPA] Site Num: ", Site.Num, ", ",
                             "County Name: ", County.Name)) %>%
   addCircles(data = unique(select(aeronet.sites, 
                                   c(Longitude.decimal_degrees.,
@@ -26,13 +26,27 @@ leaflet(unique(select(subset(hourly.pm25.FRM.14_17), c(Longitude, Latitude, Site
              lng = ~Longitude.decimal_degrees.,
              lat = ~Latitude.decimal_degrees.,
              color = "red",
-             label = ~paste("Site Name: ", Site_Name)) %>%
+             label = ~paste("[AERONET] Site Name: ", Site_Name)) %>%
   addTiles() %>%
   addProviderTiles(providers$CartoDB.Positron) %>%
   addScaleBar()
 
-get.summary <- function(site_num, county_name) {
-  summary(subset(hourly.pm25.FRM.14_17, Site.Num = site_num, County.Name = county_name))
+get.site_methods <- function(site_num, county_name) {
+  unique(select(subset(hourly.pm25.FRM.14_17, Site.Num == site_num &
+                County.Name == county_name),
+         c(Site.Num, County.Name, Method.Code, Method.Type, Method.Name, POC)))
 }
 
-get.summary(1001, "San Mateo")
+get.site_methods(306, "San Bernardino")
+get.site_methods(5, "Santa Clara")
+get.site_methods(6, "Santa Clara")
+
+get.site_methods(16, "Washoe")
+get.site_methods(22, "Washoe")
+get.site_methods(1005, "Washoe")
+get.site_methods(1007, "Washoe")
+
+tempdf <- bind_rows(subset(hourly.pm25.FRM.14_17, Site.Num == 16 & County.Name == "Washoe"),
+                    subset(hourly.pm25.FRM.14_17, Site.Num == 22 & County.Name == "Washoe"))
+tempdf %>%
+  ggplot
--- code/plot_functions.R
+++ code/plot_functions.R
@@ -16,7 +16,7 @@ plot.linechart.pm25 <- function(data, start.time, end.time, data.method.code) {
   data %>%
     subset(DateTime.Local >= start.time & DateTime.Local <= end.time  & Method.Code == data.method.code) %>%
     ggplot(aes(x = DateTime.Local, y = Sample.Measurement, group = Site.Num, color = as.character(Site.Num))) +
-    geom_point(position = position_dodge(width = 0.75)) +
+    geom_line() +
     geom_smooth(method = "loess", se = FALSE, linetype = 2, span = 0.2, aes(group = 1)) +
     theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
     labs(x = "Time", y = "Micrograms/cubic meter", color = "Sites") +
@@ -30,3 +30,10 @@ plot.linechart.AOD <- function(data, start.time, end.time) {
     ggplot(aes(x = DateTime.GMT, y = X440.870_Angstrom_Exponent)) +
     geom_point(aes(color = DateTime.GMT))
 }
+
+tempdf <- subset(hourly.pm25.FRM.14_17, County.Name == "San Bernardino" & Site.Num == 306)
+
+tempdf %>%
+  subset(DateTime.Local >= "2016-05-01" & DateTime.Local <= "2016-12-31") %>%
+  ggplot() +
+  geom_point(aes(x = POC, y = Sample.Measurement, color = as.character(POC)))
